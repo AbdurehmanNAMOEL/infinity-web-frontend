a4c0fbf9371d0c175db1330d83d35fa854157f48
Abdurehman Saeed <asaeed526@gmail.com>
2023-03-10 19:42:22 +0300

    lading page added

--- src/shared/Components/LoadingPage.jsx
+++ src/shared/Components/LoadingPage.jsx
@@ -1,13 +1,21 @@
-import { Box } from '@mui/material'
+import { Box, CardMedia } from '@mui/material'
 import React from 'react'
 import LoadingAnimation from './LoadingAnimation'
-
+import logo from '../../assets/image/logo.png'
 const LoadingPage = () => {
 
     return (
     <>
     <Box sx={style.loadingPageContainer}>
-      <LoadingAnimation/>   
+      <CardMedia image={logo} sx={{width:'150px',height:'160px'}}/>
+      <Box sx={{
+        marginTop:'10px',
+        width:'100px',
+        height:'50px',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+        }}><LoadingAnimation/></Box>    
     </Box>
     </>
   )
@@ -22,7 +30,8 @@ const style= {
     justifyContent:'center',
     alignItems:'center',
     position:'absolute',
-    zIndex:4000
+    zIndex:4000,
+    flexDirection:'column'
   }
 }
 
